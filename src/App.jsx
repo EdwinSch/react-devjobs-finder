f725ab640c69b75e79a24f6e8261bce810d66c84
EdwinSch <edwinschutjes@gmail.com>
2024-02-07 11:34:07 +0100

    setup fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+// import jobs from "../api/data.json";
+
+import { useState, useEffect } from "react";
+
+const url = "../api/data.json";
+
 function App() {
-  return <h1>hello world</h1>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Fetch from API
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setJobs(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  console.log(jobs);
+
+  // Invoke Fetch
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      {jobs.map((job) => {
+        return <p key={job.id}>{job.company}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
